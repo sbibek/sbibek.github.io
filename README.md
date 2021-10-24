58940642674c167955a0cb0445e23df8677a3811
Bibek Shrestha <sbibek@hotmail.com>
2021-10-24 11:55:56 -0700

    prev

---
+++ README.md
@@ -0,0 +1,1 @@
+# sbibek.github.io
--- _next/static/chunks/78e521c3-c9cfc7c3820dfc5eda11.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[937],{9352:function(t,a,n){n.d(a,{GfL:function(){return r}});var h=n(4405);function r(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M18 18v2H6v-2h12zm3-7v2H3v-2h18zm-3-7v2H6V4h12z"}}]}]})(t)}}}]);
